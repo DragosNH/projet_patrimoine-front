8c5f3244b1b95b9d40029a6e07255a22a79e5947
Dragos <edragos33@gmail.com>
2025-04-28 09:33:01 +0200

    ARCore forced added

---
+++ Extra functionalities/ForceARCore .cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ForceARCore : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(CheckARSupport());
+    }
+
+    System.Collections.IEnumerator CheckARSupport()
+    {
+        yield return ARSession.CheckAvailability();
+
+        if (ARSession.state == ARSessionState.Unsupported)
+        {
+            Debug.LogWarning("ARCore not officially supported. Trying to force start anyway...");
+        }
+        else if (ARSession.state == ARSessionState.NeedsInstall)
+        {
+            yield return ARSession.Install();
+        }
+
+        if (ARSession.state == ARSessionState.Ready)
+        {
+            Debug.Log("Starting AR Session manually.");
+            var arSession = FindObjectOfType<ARSession>();
+            if (arSession != null)
+            {
+                arSession.enabled = true;
+            }
+        }
+    }
+}
