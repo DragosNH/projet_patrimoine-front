2d65356dc6647d5485ba124c45fe084bd5d337d5
Dragos <edragos33@gmail.com>
2025-06-10 14:02:22 +0200

    small changes

--- AtticLoader.cs
+++ AtticLoader.cs
@@ -3,7 +3,7 @@ using UnityEngine.Networking;
 using System.Collections;
 using Newtonsoft.Json;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.XR.ARFoundation;
 
 [System.Serializable]
 public class AtticData
@@ -21,19 +21,19 @@ public class AtticListWrapper
 
 public class AtticLoader : MonoBehaviour
 {
+    [SerializeField] ARSessionOrigin arOrigin; 
+
     string apiUrl = NetworkConfig.ServerIP + "/api/attic-skeleton/";
 
     void Start()
     {
-        // Keep the screen on as long as this scene is running
         Screen.sleepTimeout = SleepTimeout.NeverSleep;
-
         StartCoroutine(FetchAndLoadAttic());
     }
 
     IEnumerator FetchAndLoadAttic()
     {
-        UnityWebRequest req = UnityWebRequest.Get(apiUrl);
+        var req = UnityWebRequest.Get(apiUrl);
         req.SetRequestHeader("Authorization", "Bearer " + PlayerPrefs.GetString("access_token"));
         yield return req.SendWebRequest();
 
@@ -54,16 +54,16 @@ public class AtticLoader : MonoBehaviour
 
         string bundleUrl = data.atticSkeletons[0].file;
 
-        UnityWebRequest bundleRequest = UnityWebRequestAssetBundle.GetAssetBundle(bundleUrl);
-        yield return bundleRequest.SendWebRequest();
+        var bundleReq = UnityWebRequestAssetBundle.GetAssetBundle(bundleUrl);
+        yield return bundleReq.SendWebRequest();
 
-        if (bundleRequest.result != UnityWebRequest.Result.Success)
+        if (bundleReq.result != UnityWebRequest.Result.Success)
         {
-            Debug.LogError("Failed to download AssetBundle: " + bundleRequest.error);
+            Debug.LogError("Failed to download AssetBundle: " + bundleReq.error);
             yield break;
         }
 
-        AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(bundleRequest);
+        AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(bundleReq);
         if (bundle == null)
         {
             Debug.LogError("AssetBundle is null.");
@@ -72,6 +72,7 @@ public class AtticLoader : MonoBehaviour
 
         string assetName = bundle.GetAllAssetNames()[0];
         GameObject prefab = bundle.LoadAsset<GameObject>(assetName);
+        bundle.Unload(false);
 
         if (prefab == null)
         {
@@ -79,21 +80,23 @@ public class AtticLoader : MonoBehaviour
             yield break;
         }
 
-        GameObject attic = Instantiate(prefab);
-        // attic.transform.position = new Vector3(0f, 0f, 2f);
-        Transform cam = Camera.main.transform;
-        Vector3 spawnPosition = cam.position + (cam.forward * 1.5f) + new Vector3(0f, -0.5f, 0f);
-        attic.transform.position = spawnPosition;
-        attic.transform.rotation = Quaternion.LookRotation(-cam.forward);
+        // Instantiate under ARSessionOrigin so it moves with your AR camera
+        GameObject attic = Instantiate(
+            prefab,
+            Vector3.zero,
+            Quaternion.identity,
+            arOrigin.trackablesParent);
 
-
-
-        bundle.Unload(false);
+        // Position it 1.5m in front of the camera, 0.5m up
+        Transform cam = arOrigin.camera.transform;
+        Vector3 forward = cam.forward;
+        forward.y = 0f;
+        attic.transform.localPosition = forward * 1.5f + Vector3.up * 0.5f;
+        attic.transform.localRotation = Quaternion.LookRotation(-forward);
     }
 
     public void Return()
     {
         SceneManager.LoadScene("MainPage");
     }
-    
 }
